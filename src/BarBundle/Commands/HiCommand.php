1a7fca87008e6c513bfffcfef130c2287d29ad5d
Maykel S. Braz <maykelsb@yahoo.com.br>
2016-08-10 11:11:55 -0300

    Evaluation commands

--- src/BarBundle/Commands/HiCommand.php
+++ src/BarBundle/Commands/HiCommand.php
@@ -1,8 +1,36 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * A simple command to evaluate ChainingCommand implementation.
+ *
+ * @author Maykel S. Braz <maykelsb@yahoo.com.br>
  */
 
+namespace BarBundle\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ *
+ */
+class HiCommand extends Command
+{
+
+    /**
+     * Configures the hi command.
+     *
+     * Setting its name, description and help message.
+     */
+    protected function configure()
+    {
+        $this->setName('bar:hi')
+            ->setDescription('Use to say hi.')
+            ->setHelp('This command allows you to say hi from Bar.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('Hi from Bar!');
+    }
+}
--- src/FooBundle/Commands/HelloCommand.php
+++ src/FooBundle/Commands/HelloCommand.php
@@ -1,8 +1,36 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * A simple command to evaluate ChainingCommand implementation.
+ *
+ * @author Maykel S. Braz <maykelsb@yahoo.com.br>
  */
 
+namespace FooBundle\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ *
+ */
+class HelloCommand extends Command
+{
+
+    /**
+     * Configures the hello command.
+     *
+     * Setting its name, description and help message.
+     */
+    protected function configure()
+    {
+        $this->setName('foo:hello')
+            ->setDescription('Use to say hello.')
+            ->setHelp('This command allows you to say hello from Foo.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('Hello from Foo!');
+    }
+}
