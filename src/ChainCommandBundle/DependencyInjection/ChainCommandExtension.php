12ff724416b90b6b0a24e1c0d4f0f6c585f38470
Maykel S. Braz <maykelsb@yahoo.com.br>
2016-08-13 16:45:20 -0300

    A bundle extension to load its service.yml files

---
+++ src/ChainCommandBundle/DependencyInjection/ChainCommandExtension.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * This code is part of my solution to Console Command Chaining.
+ *
+ * @link https://github.com/mbessolov/test-tasks/blob/master/7.md
+ */
+
+namespace ChainCommandBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+/**
+ * This is a bundle extension to load this bundle services.
+ *
+ * @author Maykel S. Braz <maykelsb@yahoo.com.br>
+ */
+class ChainCommandExtension extends Extension
+{
+    /**
+     * Loads @ChainCommandBundle/Resources/config/services.yml
+     *
+     * @param array $configs
+     * @param ContainerBuilder $container
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__ . '/../Resources/config')
+        );
+
+        $loader->load('services.yml');
+    }
+
+}
