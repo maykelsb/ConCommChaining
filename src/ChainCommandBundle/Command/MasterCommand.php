494d00a146d5032d51d2f7e0ed09e261766a1116
Maykel S. Braz <maykelsb@yahoo.com.br>
2016-08-14 06:04:27 -0300

    Setting log to log_ccc.log file, fixing log of console output

--- src/ChainCommandBundle/Command/MasterCommand.php
+++ src/ChainCommandBundle/Command/MasterCommand.php
@@ -8,9 +8,11 @@
 namespace ChainCommandBundle\Command;
 
 use ChainCommandBundle\DependencyInjection\Compiler\CommandChainPass;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Output\StreamOutput;
 
 /**
  * This is a master command which is used to call a chain of commands.
@@ -20,6 +22,8 @@ use Symfony\Component\Console\Output\OutputInterface;
  *
  * The switch between this class and the main command occurs in
  * CommandChainPass::activeMainCommandChain().
+ *
+ * @todo Convert to thin command
  */
 class MasterCommand extends ContainerAwareCommand
 {
@@ -34,14 +38,24 @@ class MasterCommand extends ContainerAwareCommand
     protected $chainedCommands = [];
 
     /**
-     * @var Psr\Log\LoggerInterface Logger interface.
+     * @var LoggerInterface Logger interface.
      */
     protected $logger;
 
+    /**
+     * @var StreamOutput Save output in memory to write in log later.
+     */
+    protected $outputMemory;
+
+    /**
+     * Returns a reference of LoggerInterface.
+     *
+     * @return LoggerInterface
+     */
     protected function getLogger()
     {
         if (is_null($this->logger)) {
-            $this->logger = $this->getContainer()->get('logger');
+            $this->logger = $this->getContainer()->get('monolog.logger.ccc');
         }
 
         return $this->logger;
@@ -51,7 +65,7 @@ class MasterCommand extends ContainerAwareCommand
      * Defines the id of the command service.
      *
      * @param string $commandId Service id of main command.
-     * @return \ChainCommandBundle\Command\MasterCommand
+     * @return MasterCommand
      */
     public function setMainCommand($commandId)
     {
@@ -63,7 +77,7 @@ class MasterCommand extends ContainerAwareCommand
      * Defines the list of chained commands.
      *
      * @param string[] $chainedCommands List of chained commands.
-     * @return \ChainCommandBundle\Command\MasterCommand
+     * @return MasterCommand
      */
     public function setChainedCommands(array $chainedCommands)
     {
@@ -87,22 +101,52 @@ class MasterCommand extends ContainerAwareCommand
      * @param InputInterface $input Common input interface.
      * @param OutputInterface $output Common output interface.
      */
-    protected function execute(InputInterface $input, OutputInterface $output) {
-        $container = $this->getContainer();
-
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // -- Executing main command
         $this->logHeader();
+        $this->executeCommand(
+            $this->mainCommand . CommandChainPass::MAINCOMM_POSFIX,
+            $input,
+            $output
+        );
 
-        $container->get($this->mainCommand . CommandChainPass::MAINCOMM_POSFIX)
-            ->execute($input, $output);
+        // -- Executing chain members
         $this->getLogger()->info("Executing {$this->getName()} chain members:");
-
         foreach ($this->chainedCommands as $commandInfo) {
-            $container
-                ->get($commandInfo['serviceid'] . CommandChainPass::CHAINEDCOMM_POSFIX)
-                ->execute($input, $output);
+            $this->executeCommand(
+                $commandInfo['serviceid'] . CommandChainPass::CHAINEDCOMM_POSFIX,
+                $input,
+                $output
+            );
         }
-
         $this->getLogger()->info("Execution of {$this->getName()} chain completed.");
+    }
+
+    /**
+     * Execute a command and stores its output in log before show it on screen.
+     *
+     * @param string $serviceId Service id to be called.
+     * @param InputInterface $input Common input interface.
+     * @param OutputInterface $output Common output interface.
+     */
+    protected function executeCommand(
+        $serviceId,
+        InputInterface $input,
+        OutputInterface $output
+    ) {
+        // -- Storing ouput of the command in memory
+        $memoryOutput = new StreamOutput(fopen('php://memory', 'w', false));
+        $this->getContainer()->get($serviceId)->execute($input, $memoryOutput);
+
+        // -- Preparing the memory buffer to read the last message
+        rewind($memoryOutput->getStream());
+
+        // -- Logging the last message
+        $this->getLogger()->info($text = stream_get_contents($memoryOutput->getStream()));
+
+        // -- Writing it on screen
+        $output->writeln(trim($text));
     }
 
     /**
